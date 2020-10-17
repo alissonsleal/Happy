f29628c7a5404ecdd7b4c2bf6cb0ccf41f0f3dd5
Alisson <alissonsleal@hotmail.com>
2020-10-17 15:00:52 -0300

    we're using postgres instead of sqlite

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,20 @@
-import express from 'express'
-import path from 'path'
-import 'express-async-errors'
-import cors from 'cors'
+import "dotenv/config";
+import express from "express";
+import path from "path";
+import "express-async-errors";
+import cors from "cors";
 
-import './database/connection'
-import errorHandler from './errors/handler'
+import "./database/connection";
+import errorHandler from "./errors/handler";
 
-import OrphanageRouter from './routes/routes'
+import OrphanageRouter from "./routes/routes";
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
-app.use(OrphanageRouter)
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
-app.use(errorHandler)
+app.use(cors());
+app.use(express.json());
+app.use(OrphanageRouter);
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(errorHandler);
 
-app.listen(4000) 
+app.listen(process.env.PORT || 3333);
